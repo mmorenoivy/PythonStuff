1a6317bde2492b9c7f79e18c25525fdc3317750b
Marj <marjorie_moreno@hotmail.com>
2017-08-18 14:11:05 -0500

    Add files via upload
    created for python flask tutorial

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'I am a body',
+			'author': 'Marjorie',
+			'create_date'; '18-08-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'I am a body',
+			'author': 'Marjorie',
+			'create_date'; '18-08-2017'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'I am a body',
+			'author': 'Marjorie',
+			'create_date'; '18-08-2017'
+		}
+	]
+	return articles
